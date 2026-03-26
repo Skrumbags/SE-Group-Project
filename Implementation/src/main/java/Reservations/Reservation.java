85a775d7f0d7aaa04d315f647b73e3c923eed16b
Emmanuel Humber <emmanuelhumber@gmail.com>
2026-03-25 20:08:13 -0500

    Implemented Reserve Room Validation, Reserve Room Summary, and Finished Reserve Room Use Case

--- Implementation/src/main/java/Reservations/Reservation.java
+++ Implementation/src/main/java/Reservations/Reservation.java
@@ -1,26 +1,62 @@
-/*
- *  Filename: Reservation.java
- *  Date Created: 3/24/2026
- *  Date Last Modified: 3/24/2026
- *  Authors: Matt Freeman, XXX
- *  File Description:
- *      XXXX
- */
-
-package Reservations;
-
-import Rooms.Room;
-import Utility.DateRange;
-
-public class Reservation {
-    private Room room;
-    private DateRange dateRange;
-
-    public Reservation(Room room, DateRange dateRange) {
-        this.room = room;
-        this.dateRange = dateRange;
-    }
-
-    public Room getRoom() { return room; }
-    public DateRange getDateRange() { return dateRange; }
-}
+/*
+ *  Filename: Reservation.java
+ *  Date Created: 3/24/2026
+ *  Date Last Modified: 3/25/2026
+ *  Authors: Matt Freeman, XXX
+ *  File Description:
+ *      A confirmed reservation with guest details and confirmation number.
+ */
+
+package Reservations;
+
+import Rooms.Room;
+import Utility.DateRange;
+
+public class Reservation {
+    private final String confirmationNumber;
+    private final Room room;
+    private final DateRange dateRange;
+    private final String guestName;
+    private final String guestAddress;
+    private final String maskedCardNumber;
+    private final double totalCost;
+
+    public Reservation(String confirmationNumber, Room room, DateRange dateRange,
+                       String guestName, String guestAddress, String maskedCardNumber, double totalCost) {
+        this.confirmationNumber = confirmationNumber;
+        this.room = room;
+        this.dateRange = dateRange;
+        this.guestName = guestName;
+        this.guestAddress = guestAddress;
+        this.maskedCardNumber = maskedCardNumber;
+        this.totalCost = totalCost;
+    }
+
+    public String getConfirmationNumber() {
+        return confirmationNumber;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public DateRange getDateRange() {
+        return dateRange;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getGuestAddress() {
+        return guestAddress;
+    }
+
+    public String getMaskedCardNumber() {
+        return maskedCardNumber;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+}
--- Implementation/src/main/java/Utility/ReservationService.java
+++ Implementation/src/main/java/Utility/ReservationService.java
@@ -1,20 +1,24 @@
 /*
  *  Filename: ReservationService.java
  *  Date Created: 3/24/2026
- *  Date Last Modified: 3/24/2026
+ *  Date Last Modified: 3/25/2026
  *  Authors: Matt Freeman, XXX
  *  File Description:
- *      XXXX
+ *      Tracks reservations and answers availability queries for date ranges.
  */
 
 package Utility;
 
 import Reservations.Reservation;
 import Rooms.Room;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class ReservationService {
-    List<Reservation> reservationList;
+    private final List<Reservation> reservationList = new ArrayList<>();
+    private final AtomicLong confirmationSeq = new AtomicLong(10_000);
 
     public List<Room> calculateOverlap(List<Room> rooms, DateRange dateRange) {
         return rooms.stream()
@@ -22,7 +26,6 @@ public class ReservationService {
                 .toList();
     }
 
-    /// Need to override Room::equals
     public boolean isReserved(Room room, DateRange dateRange) {
         for (Reservation res : reservationList) {
             if (room.equals(res.getRoom()) && dateRange.overlaps(res.getDateRange())) {
@@ -30,5 +33,17 @@ public class ReservationService {
             }
         }
         return false;
+    }
+
+    public void addReservation(Reservation reservation) {
+        reservationList.add(reservation);
+    }
+
+    public String nextConfirmationNumber() {
+        return "CONF-" + confirmationSeq.getAndIncrement();
+    }
+
+    public List<Reservation> getReservations() {
+        return List.copyOf(reservationList);
     }
 }
