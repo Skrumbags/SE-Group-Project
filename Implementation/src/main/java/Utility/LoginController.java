19ba60c92238540a6448cf2898cd2a087d7f9650
HannahRoss031 <hnross4557@gmail.com>
2026-04-12 18:09:41 -0500

    login constructor and UI creation

--- Implementation/src/main/java/Utility/LoginController.java
+++ Implementation/src/main/java/Utility/LoginController.java
@@ -1,4 +1,20 @@
 package Utility;
 
+import People.User;
+import People.UserCatalog;
+
 public class LoginController {
+    public UserCatalog userCatalog;
+
+    public LoginController(UserCatalog userCatalog) {
+        this.userCatalog = userCatalog;
+    }
+
+    // Will return user if in catalog, null if not
+    public User login(String username, String password) {
+        User user = userCatalog.findByUsername(username);
+        if (user == null) return null;
+        if (!user.checkPassword(password)) return null;
+        return user;
+    }
 }
