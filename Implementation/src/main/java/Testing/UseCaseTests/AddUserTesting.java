b2da699a364942976f550eda673b1c822ccfe770
Andrew Hutcheson <andrewhutch10@gmail.com>
2026-05-09 19:14:57 -0500

    delete old tests

--- Implementation/src/main/java/Testing/UseCaseTests/AddUserTesting.java
+++
@@ -1,9 +0,0 @@
-package Testing.UseCaseTests;
-
-import Testing.UseCases.AddUser;
-
-public class AddUserTesting {
-    public static void main(String[] args) {
-        AddUser.testWithoutUI1();
-    }
-}
